a790323591ce9c6be4ca9e3c278f6c23bcf6780a
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-05 09:37:28 -0700

    ReflectionProblem

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System.Reflection;
+
+namespace ReflectionPracticeProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Type c = typeof(EnterPersonDetails);
+
+            ConstructorInfo[] constructorInfos = c.GetConstructors();
+
+            foreach(ConstructorInfo ci in constructorInfos)
+            {
+                Console.WriteLine(ci.Name);
+            }
+
+            MethodInfo[] methodInfos = c.GetMethods();
+
+            foreach(MethodInfo ci in methodInfos)
+
+                {
+                Console.WriteLine(ci);
+            }
+        }
+    }
+}
